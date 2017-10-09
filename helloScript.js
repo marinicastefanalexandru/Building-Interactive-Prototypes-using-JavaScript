b6c6e39f8f11a1e05ca018237bcbffa0e69223a2
Stefan Marinica <marinica.stefan@outlook.com>
2017-10-09 19:32:15 +0300

    update3

--- helloScript.js
+++ helloScript.js
@@ -4,20 +4,6 @@ window.alert(msg);
 console.log("Hello, console!");
 document.write("Hello, document!");
 
-document.write("<br/>");
-
-document.write("Welcome to Number Fun", "<br/>");
-
-var operand1 = prompt("Operand1");
-var operand2 = prompt("Operand2");
-
-document.write("Operand#1: ", operand1, " ", "Operand#2: ", operand2, "<br/>");
-
-document.write("The sum of ", operand1, " and ", operand2, " is ", operand1 + operand2, "<br/>");
-document.write("The difference of ", operand1, " and ", operand2, " is ", operand1 - operand2, "<br/>");
-document.write("The product of ", operand1, " and ", operand2, " is ", operand1 * operand2, "<br/>");
-document.write("The quotient of ", operand1, " and ", operand2, " is ", operand1 / operand2, "<br/>");
-document.write("The result of the mod operation ", operand1, " and ", operand2, " is ", operand1 % operand2, "<br/>");
 
 
 
