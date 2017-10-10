194795c18d2a77363f70a791eeba560b7c3ecebf
marinicastefanalexandru <32274391+marinicastefanalexandru@users.noreply.github.com>
2017-10-10 11:54:27 +0300

    Update lab.js

--- Module1lab/lab.js
+++ Module1lab/lab.js
@@ -2,3 +2,6 @@
 var userName = window.prompt("Please enter your name:");
 var greetingParagraph = document.getElementById("greeting");
 greetingParagraph.innerHTML += ", " + userName;
+
+var num1 = window.prompt("Enter first number: ");
+var num2 = window.prompt("Enter second number: ");
